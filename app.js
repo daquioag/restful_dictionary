dfd30ba2d5fa814695cb614b14f817101b98b4ab
daquioag <aldaquioag@gmail.com>
2023-10-06 01:51:26 -0700

    implement search client and server side

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const PORT = 3000;
 // Data structure to store dictionary entries
 const dictionary = [];
 let number_of_requests = 0;
-
+function printDictionary() {
+    for (const entry of dictionary) {
+        console.log(`Word: ${entry.word}, Definition: ${entry.definition}`);
+    }
+}
 const validateInput = (word, definition) => {
   if (!word || !definition) {
     return false;
@@ -24,6 +28,7 @@ const server = http.createServer((req, res) => {
   console.log("The server received a request!");
   const parsedUrl = url.parse(req.url);
   const pathName = parsedUrl.pathname;
+  console.log(pathName);
 
   // Enable CORS (Cross-Origin Resource Sharing) for all routes
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -38,6 +43,7 @@ const server = http.createServer((req, res) => {
   }
 
   if (pathName === "/store" && req.method === "POST") {
+    console.log(pathName)
     let body = "";
     console.log("POOOOOST1");
     req.on("data", (chunk) => {
@@ -57,7 +63,7 @@ const server = http.createServer((req, res) => {
 
         number_of_requests++;
         dictionary.push({ word, definition });
-
+        printDictionary();
         res.writeHead(200, { "Content-Type": "application/json" });
         console.log("POOOOOST6");
         res.end(
@@ -77,28 +83,32 @@ const server = http.createServer((req, res) => {
         );
       }
     });
-  } else if (pathName.startsWith("/search/") && req.method === "GET") {
+  } else if (pathName.includes("/search") && req.method === "GET") {
+    console.log("get0");
     const searchWord = pathName.slice("/search/".length).toLowerCase();
-
+    console.log(searchWord);
+    console.log(pathName);
+    printDictionary()
     // const definition = dictionary.find(
     //   (Object) => Object.word.toLowerCase() === searchWord
     // );
+    console.log("get1");
 
     let definition;
-    let word;
+    let word_and_definition;
     for (const object of dictionary) {
       if (object.word.toLowerCase() === searchWord) {
         definition = object.definition;
         word = object.word;
+        word_and_definition = object;
         break; // Once found, exit the loop
       }
     }
 
-    if (searchWord in dictionary) {
-      const definition = dictionary[searchWord];
+    if (word_and_definition) {
       res.writeHead(200, { "Content-Type": "application/json" });
       number_of_requests++;
-      res.end(JSON.stringify(definition));
+      res.end(JSON.stringify(word_and_definition));
     } else {
       res.writeHead(404, { "Content-Type": "application/json" });
       res.end(JSON.stringify({ error: `Word '${searchWord}' not found!` }));
